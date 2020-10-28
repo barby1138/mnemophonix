38223da9b13fd23e68b45ac9672e0fb8538ce6ab
tsis <tsis@tsis.com>
2020-10-28 16:32:02 +0100

    update

--- search.c
+++ search.c
@@ -89,7 +89,7 @@ int search(struct signatures* sample, struct index* database, struct lsh* lsh) {
 
         int res = get_matches(lsh, sample->signatures[i].minhash, &list);
         if (res > MAX_MATCHES_CNT) {
-            printf("too many matches_cnt %ld\n", res);
+            printf("too many matches_cnt %d\n", res);
             free_signature_list(list);
             return MEMORY_ERROR;
         }
